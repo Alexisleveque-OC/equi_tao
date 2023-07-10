5ebacf5ae4b743ad8a3576ae55476f11f8e93e6e
alexis leveque <alexis.leveque78@gmail.com>
2023-07-10 16:35:09 +0200

    delete useless "use"

--- src/Form/UpdateUserForAdminType.php
+++ src/Form/UpdateUserForAdminType.php
@@ -2,15 +2,16 @@
 
 namespace App\Form;
 
+use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class UpdateUserType extends AbstractType
+class UpdateUserForAdminType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -19,15 +20,15 @@ class UpdateUserType extends AbstractType
                 'constraints' => new NotBlank(),
             ])
             ->add('username')
-            ->add('password_old', PasswordType::class, [
-                'label' => 'Mot de passe actuel',
+            ->add('password', TextType::class, [
+                'password' => ['label' => 'Mot de passe actuel'],
                 'required' => false
             ])
-            ->add('password_new', RepeatedType::class, [
-                    'type' => \Symfony\Component\Form\Extension\Core\Type\PasswordType::class,
-                    'first_options' => ['label' => 'Nouveau mot de passe', 'attr' => ['placeholder' => 'Mot de passe']],
-                    'second_options' => ['label' => 'Confirmation du nouveau mot de passe', 'attr' => ['placeholder' => 'Confirmation du mot de passe']],
-                    'required' => false]
+            ->add('password', RepeatedType::class, [
+                'type' => \Symfony\Component\Form\Extension\Core\Type\PasswordType::class,
+                'first_options' => ['label' => 'Nouveau mot de passe', 'attr' => ['placeholder' => 'Nouveau mot de passe']],
+                'second_options' => ['label' => 'Confirmation du nouveau mot de passe', 'attr' => ['placeholder' => 'Confirmation du nouveau mot de passe']],
+                'required' => false]
             );
 
     }
@@ -35,6 +36,7 @@ class UpdateUserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'data_class' => User::class,
         ]);
     }
 }
